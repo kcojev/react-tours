63c82d8d34468417a7ae53c041600e7fcc27f335
Georgy Kcojev <kcojev@gmail.com>
2023-01-15 19:20:43 +0300

    remove tour button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setLoading(true)
     try {
@@ -38,7 +43,7 @@ function App() {
 
   return (
     <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
